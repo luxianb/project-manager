a9c44fa707d80010f9f7ed8a69710550576a6b59
luxianb <40238895+luxianb@users.noreply.github.com>
2021-12-22 15:00:29 +0800

    Update signup, login styling

--- pages/login.js
+++ pages/login.js
@@ -1,33 +1,59 @@
 import axios from "axios";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
+import { Col, Page } from "../src/components/containers";
+import Navbar from "../src/layouts/Navbar";
 
 export default function LogInPage() {
-  const router = useRouter()
-  const [form, setForm] = useState({email: '', password: ''})
+  const router = useRouter();
+  const [form, setForm] = useState({ email: "", password: "" });
+  const [error, setError] = useState(false);
 
   async function handleLogIn() {
-    const res = await axios.post('/api/login', form);
-    console.log(res);
-    if (res.status === 200) {
-      router.push('/')
+    try {
+      const res = await axios.post("/api/login", form);
+      console.log(res);
+      if (res.status === 200) {
+        router.push("/");
+      }
+    } catch (error) {
+      setError(true);
     }
   }
 
-  return(
-    <div>
-      <input
-        placeholder="email"
-        value={form.email}
-        onChange={e => setForm({...form, email: e.target.value})}
-      />
-      <input
-        placeholder="password"
-        type='password'
-        value={form.password}
-        onChange={e => setForm({...form, password: e.target.value})}
-      />
-      <button onClick={handleLogIn}>Log in</button>
-    </div>
-  )
+  return (
+    <Page>
+      <Navbar />
+      <Col style={{ gap: 12, alignItems: "center", padding: "32px 18px" }}>
+
+        <Col>
+          <label htmlFor="email">Email:</label>
+          <input
+            id="email"
+            placeholder="email"
+            value={form.email}
+            onChange={(e) => setForm({ ...form, email: e.target.value })}
+            style={{ width: 200 }}
+          />
+        </Col>
+
+        <Col>
+          <label htmlFor="password">Password:</label>
+          <input
+            id="password"
+            placeholder="password"
+            type="password"
+            value={form.password}
+            onChange={(e) => setForm({ ...form, password: e.target.value })}
+            style={{ width: 200 }}
+          />
+          {error && (<p style={{color: 'red', maxWidth: 200, fontSize: '.8rem'}}>Unable to login, make sure email and password is correct</p>)}
+        </Col>
+
+        <button onClick={handleLogIn} style={{ width: 200 }}>
+          Log in
+        </button>
+      </Col>
+    </Page>
+  );
 }
--- pages/signup.js
+++ pages/signup.js
@@ -1,28 +1,45 @@
 import axios from "axios";
 import React, { useState } from "react";
+import { Col, Page } from "../src/components/containers";
+import Navbar from "../src/layouts/Navbar";
 
 export default function SignUpPage() {
-  const [form, setForm] = useState({email: '', password: ''})
+  const [form, setForm] = useState({ email: "", password: "" });
 
   async function handleSignUp() {
-    const res = await axios.post('/api/signup', form);
+    const res = await axios.post("/api/signup", form);
     console.log(res);
   }
 
-  return(
-    <div>
-      <input
-        placeholder="email"
-        value={form.email}
-        onChange={e => setForm({...form, email: e.target.value})}
-      />
-      <input
-        placeholder="password"
-        type='password'
-        value={form.password}
-        onChange={e => setForm({...form, password: e.target.value})}
-      />
-      <button onClick={handleSignUp}>Sign Up</button>
-    </div>
-  )
+  return (
+    <Page>
+      <Navbar />
+      <Col style={{ gap: 12, alignItems: "center", padding: "32px 18px" }}>
+        <Col>
+          <label htmlFor="email">Email:</label>
+          <input
+            id="email"
+            placeholder="email"
+            value={form.email}
+            onChange={(e) => setForm({ ...form, email: e.target.value })}
+            autoComplete="new-email"
+            style={{minWidth: 200}}
+            />
+        </Col>
+        <Col>
+          <label htmlFor="password">Password:</label>
+          <input
+            id="password"
+            placeholder="password"
+            type="password"
+            value={form.password}
+            onChange={(e) => setForm({ ...form, password: e.target.value })}
+            autoComplete="new-password"
+            style={{minWidth: 200}}
+            />
+        </Col>
+        <button onClick={handleSignUp} style={{minWidth: 200}}>Sign Up</button>
+      </Col>
+    </Page>
+  );
 }
